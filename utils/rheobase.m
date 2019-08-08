69ac6c199e45eb2061c13c4e226d1372c3c504e8
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-08 13:19:51 -0400

    Complete overhaul of the function to use a for loop

--- utils/rheobase.m
+++ utils/rheobase.m
@@ -5,64 +5,54 @@
 %% Arguments:
 %   x: the xolotl object
 %   min_firing_rate: the minimum acceptable firing rate to terminate the simulations (Hz)
-%   current_step: the current step to use to determine the rheobase (nA)
+%   current_steps: a vector of current steps to determine the rheobase
 %   verbosity: print extra information?
 %   varargin: arguments to xtools.V2metrics
+
 %% Outputs:
 %   I_ext: the determined rheobase in nA
-%% Example
-%   I_ext = rheobase(x, 0.5, 0.01, 'sampling_rate', 1/x.dt)
+%   ii: the index into current_steps for I_ext
+%   metrics: properties of the waveform computed by xtools
 
-function [I_ext, firing_rate] = rheobase(x, min_firing_rate, current_step, verbosity, varargin)
+%% Example
+%   I_ext = rheobase(x, 0.5, 0:0.01:1, 'sampling_rate', 1/x.dt)
+
+%% See Also: xtools.V2metrics
+
+function [I_ext, ii, metrics] = rheobase(x, min_firing_rate, current_steps, verbosity, varargin)
+
+  if ~exist('min_firing_rate', 'var') || isempty(min_firing_rate)
+    min_firing_rate = 1; % Hz
+  end
+
+  if ~exist('current_steps', 'var') || isempty(current_steps)
+    current_steps = 0:0.01:1; % nA
+  end
 
   if ~exist('verbosity', 'var') || isempty(verbosity)
     verbosity = true;
   end
 
-  if ~exist('current_step', 'var') || isempty(current_step)
-    current_step = 0.01; % nA
-  end
-
-  if ~exist('min_firing_rate', 'var') || isempty(min_firing_rate)
-    min_firing_rate = eps;
-
   % save the initial state
   x.snapshot('rheobase_function');
 
-  % useful variables
-  is_spiking = false;
-  x.I_ext = 0;
-  I_ext = 0;
-  counter = 0;
+  for ii = 1:length(current_steps)
+    corelib.verb(verbosity, 'rheobase', ['I_ext = ' num2str(current_steps(ii)) ' nA'])
 
-  while is_spiking == false
-    counter = counter + 1;
-    corelib.verb(verbosity, 'rheobase', ['I_ext = ' num2str(I_ext) ' nA, counter = ' num2str(counter)])
+    x.reset('rheobase_function');
+    x.closed_loop;
+    x.I_ext = current_steps(ii);
 
-    % simulate the model
-    x.reset;
-    x.closed_loop = true;
-    % discard the transient
     x.integrate;
     V = x.integrate;
 
-    % compute the firing rate
     metrics = xtools.V2metrics(V, varargin{:});
 
-    % if 100 iterations have passed or the firing rate is greater than/equal to the minimum
-    % terminate the simulation
-    % otherwise, continue
-    if metrics.firing_rate >= min_firing_rate | counter > 100;
-      is_spiking = true;
-      firing_rate = metrics.firing_rate;
-    else
-      I_ext = I_ext + current_step;
-      x.I_ext = I_ext;
+    if metrics.firing_rate >= min_firing_rate
+      break
     end
+  end % for loop
 
-  end
+  I_ext = current_steps(ii);
 
-  % return to the initial state
-  x.reset('rheobase_function');
-
-end
+end % function
