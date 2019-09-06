9458104fefb61b6e2cb6da707e617b7e243191fa
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-06 15:32:22 -0400

    Simulate the voltage in three conditions in closed loop mode

--- utils/simDecay.m
+++ utils/simDecay.m
@@ -4,29 +4,52 @@ function [cost, V, I_ext, tau_fr, costparts] = simDecay(x, ~, ~)
   % then, simulate with more current,
   % then, drop the current
 
-  % figure out the minSpikingCurrent for the model
-  I_ext = minSpikingCurrent(x, 'min_firing_rate', 1, 'verbosity', false);
+  cost        = 0;
+  costparts   = zeros(5, 1);
+  weights     = [1, 1, 1, 1, 1];
 
-  % simulate with some injected current
+  % containers
+  V           = NaN(x.t_end / x.dt, 3);
+  spiketimes  = cell(3, 1);
+  rat         = cell(3, 1);
+  mean_rat    = Nan(3, 1);
+  tau_fr      = Nan(3, 1);
+
+  %% Figure out the minimum applied current for the model
+
+  I_ext       = minSpikingCurrent(x, 'min_firing_rate', 1, 'verbosity', false);
+  I_ext_2     = minSpikingCurrent(x, 'min_firing_rate', 10, 'verbosity', false);
+
+  %% Simulate with some injected current
+
   x.reset;
   x.closed_loop = true;
   x.I_ext = I_ext;
+  % reach a tonic-spiking steady-state
   x.integrate;
-  V = x.integrate;
+  % simulate and save the voltage
+  V(:, 1) = x.integrate;
 
   % compute the spike times
-  spiketimes = veclib.nonnans(xtools.findNSpikeTimes(V - mean(V), 600, 10));
+  spiketimes{1} = veclib.nonnans(xtools.findNSpikeTimes(V(:, 1) - mean(V(:, 1)), 600, 10));
 
-  % simulate with twice the current
+  %% Simulate with much more current
+
   x.reset;
-  x.I_ext = 2 * I_ext;
-  V2 = x.integrate;
+  x.I_ext = I_ext_2;
+  V(:, 2) = x.integrate;
 
   % compute the spike times
-  spiketimes2 = veclib.nonnans(xtools.findNSpikeTimes(V2 - mean(V2), 600, 10));
+  spiketimes{2} = veclib.nonnans(xtools.findNSpikeTimes(V(:, 2) - mean(V(:, 2)), 600, 10));
 
-  % concatenate the two simulations to get the full trace
-  V = [V; V2];
+  %% Simulate once more with the normal current
+
+  x.reset
+  x.I_ext = I_ext;
+  V(:, 3) = x.integrate;
+
+  % compute the spike times
+  spiketimes{3} = veclib.nonnans(xtools.findNSpikeTimes(V(:, 3) - mean(V(:, 3)), 600, 10));
 
   %% Cost due to number of spikes
   % the number of spikes must be at least one per second of simulated time
