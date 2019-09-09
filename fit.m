2948af1de0b259b3bf66938ae54c9115da68019e
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-09 10:19:57 -0400

    Start with 100 simulations

--- fit.m
+++ fit.m
@@ -16,7 +16,7 @@ p.options.SwarmSize = 24;
 %% Initialize optimization parameters
 
 % optimization parameters
-nSims       = 2000;
+nSims       = 100;
 nEpochs     = 3;
 nParams     = length(p.parameter_names);
 
