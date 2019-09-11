dbbe98151e1b2a001661fd47f3e5a3937cfa865b
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-11 12:20:43 -0400

    Make the resulant figure prettier

--- utils/plotNModels.m
+++ utils/plotNModels.m
@@ -7,12 +7,19 @@ function plotNModels(x, cost, param_names, params, n)
   figure; hold on
 
   [~, index] = sort(cost);
+  cmap = colormaps.linspecer(n);
 
   for ii = 1:n
     x.reset;
     x.set(param_names, params(index(ii), :));
     V = x.integrate;
-    plot(V);
+    t = x.dt * (1:length(V));
+    plot(t, V, 'Color', cmap(ii, :));
   end
+
+  xlabel('time (ms)')
+  ylabel('membrane potential (mV)')
+
+  figlib.pretty('PlotBuffer', 0.2);
 
 end % function
