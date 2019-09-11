74f1b37c34d0fe5a4eccd79dfe89e8846707f6b1
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-11 12:28:19 -0400

    Use precomputed seeds

--- fit.m
+++ fit.m
@@ -1,7 +1,7 @@
 p           = xfit;
 p.x         = x;
 p.options.UseParallel = true;
-p.SimFcn    = @simSpiking;
+p.SimFcn    = @simDecay;
 
 % parameters
 param_names = [x.find('*gbar'); x.find('I_ext')];
@@ -27,9 +27,11 @@ rate        = NaN(nSims, 1);
 
 %% Fit parameters
 
+% acquire seeds
+load(['data-simSpiking-' corelib.getComputerName '.mat'])
 
 % try to load existing data file
-filename    = ['data-simSpiking-' corelib.getComputerName '.mat'];
+filename    = ['data-simDecay-' corelib.getComputerName '.mat'];
 if exist(filename)
   load(filename)
   start_idx = find(isnan(cost),1,'first')
@@ -45,7 +47,8 @@ for ii = start_idx:nSims
   try
 
     % set seed
-    p.seed = p.ub .* rand(size(p.ub));
+    % p.seed = p.ub .* rand(size(p.ub));
+    p.seed = params(ii, :);
 
     % run xfit
     for qq = 1:nEpochs
