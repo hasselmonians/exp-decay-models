d7213ae88fc22f54a6d31a6697dff4ac4f0b4fce
Alec Hoyland <alec.hoyland@posteo.net>
2020-01-27 12:14:21 -0500

    Add argument to matlab batch function call

--- batchscript.sh
+++ batchscript.sh
@@ -6,8 +6,7 @@
 #$ -P hasselmogrp
 #$ -N exp-decay-models
 #$ -l h_rt=24:00:00
-#$ -t 1-100
 
 module load matlab/2019b
 
-matlab -nodisplay -singleCompThread -r "batchFunction(ARGUMENT); exit;"
+matlab -nodisplay -singleCompThread -r "batchFunction('nSims', 100, 'nEpochs', 3); exit;"
