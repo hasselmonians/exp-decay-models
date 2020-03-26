4ee084fddd5117f2deb092d6d6f3246363f92c83
Alec Hoyland <alec.hoyland@posteo.net>
2020-03-26 13:02:46 -0400

    Try with a slightly modified model

--- models/model_soplata.m
+++ models/model_soplata.m
@@ -1,12 +1,10 @@
-x = xolotl('temperature', 36, 'temperature_ref', 22);
+x = xolotl;
 
-x.add('compartment', 'comp', 'Cm', 10, 'A', 1e-3, 'vol', 1e-6, 'Ca_out', 2e3);
+x.add('compartment', 'comp', 'Cm', 10, 'A', 1, 'vol', 1, 'Ca_out', 2e3);
 
-x.comp.add('buchholtz/CalciumMech', 'Ca_in', 0.2, 'tau_Ca', 80, 'phi', 1);
+x.comp.add('buchholtz/CalciumMech', 'Ca_in', 0.24, 'tau_Ca', 5, 'phi', 1);
 
-x.comp.add('soplata/CaN', 'gbar', 0.25);
-x.comp.add('destexhe/CaT', 'gbar', 1);
-x.comp.add('soplata/Kd', 'gbar', 200);
-x.comp.add('soplata/MCurrent', 'gbar', 7.5);
-x.comp.add('soplata/NaV', 'gbar', 2000);
-x.comp.add('Leak', 'gbar', 0.1);
+x.comp.add('soplata/NaV', 'gbar', 900, 'E', 50);
+x.comp.add('soplata/Kd', 'gbar', 100, 'E', -100);
+x.comp.add('soplata/CaT', 'gbar', 20, 'E', 30);
+x.comp.add('Leak', 'gbar', 0.1, 'E', -70);
