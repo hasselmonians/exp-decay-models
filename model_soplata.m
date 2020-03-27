4f812a38f7750ed96b605d5300fe5026eb72c924
Alec Hoyland <alec.hoyland@posteo.net>
2020-03-27 10:41:59 -0400

    Convert to function

--- model_soplata.m
+++ model_soplata.m
@@ -1,10 +1,14 @@
-x = xolotl;
+function x = model_soplata()
 
-x.add('compartment', 'comp', 'Cm', 10, 'A', 1, 'vol', 1, 'Ca_out', 2e3);
+    x = xolotl;
 
-x.comp.add('buchholtz/CalciumMech', 'Ca_in', 0.24, 'tau_Ca', 5, 'phi', 1);
+    x.add('compartment', 'comp', 'Cm', 10, 'A', 1, 'vol', 1, 'Ca_out', 2e3);
 
-x.comp.add('soplata/NaV', 'gbar', 900, 'E', 50);
-x.comp.add('soplata/Kd', 'gbar', 100, 'E', -100);
-x.comp.add('soplata/CaT', 'gbar', 20, 'E', 30);
-x.comp.add('Leak', 'gbar', 0.1, 'E', -70);
+    x.comp.add('buchholtz/CalciumMech', 'Ca_in', 0.24, 'tau_Ca', 5, 'phi', 1);
+
+    x.comp.add('soplata/NaV', 'gbar', 900, 'E', 50);
+    x.comp.add('soplata/Kd', 'gbar', 100, 'E', -100);
+    x.comp.add('soplata/CaT', 'gbar', 20, 'E', 30);
+    x.comp.add('Leak', 'gbar', 0.1, 'E', -70);
+
+end % function
